12440c4f618b52544655d4143530ea5f7818f901
bigkaiwu <bigkaiwu@163.com>
2020-05-15 14:17:20 +0800

    first commit

---
+++ src/components/common/LoginTop.vue
@@ -0,0 +1,31 @@
+<!-- 登录注册 顶部导航 -->
+<template>
+  <div>
+      <div class="LoginTop">
+          <div></div>
+          <div>{{ middleTop }}</div>
+          <div></div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['middleTop']
+}
+
+</script>
+<style lang="scss">
+.LoginTop{
+    height: 45px;
+    background-color: #fff;
+    display: flex;
+    div{
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 4.444vw;//px to vw插件 Press "Alt + Z"
+    }
+}
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,23 @@
+<!-- 注册页面  -->
+<template>
+  <div>
+      <login-top middleTop="注册Bilibili"></login-top>
+  </div>
+</template>
+
+<script>
+import LoginTop from '@/components/common/LoginTop.vue'
+
+export default {
+  data () {
+    return {
+    };
+  },
+  components: {
+      LoginTop
+  }
+}
+
+</script>
+<style scoped>
+</style>
